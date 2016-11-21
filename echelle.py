ab51d9813cc8e4485eeb21722e56fd98bcedbc29
Amalie Stokholm <stokholm@post.au.dk>
2016-11-21 21:47:39 +0100

    Fix the plot

--- echelle.py
+++ echelle.py
@@ -27,6 +27,10 @@ matplotlib_setup()
 import matplotlib.pyplot as plt
 
 
+def fix_margins():
+    plt.subplots_adjust(left=0.12, right=0.95, bottom=0.14, top=0.90)
+
+
 def kjeldsen_corr(l, n, f, q, dnu, nu0, obs_n, obs_l, obs_f, delta_nu):
     # Kjeldsen correction
     b = 4.90
@@ -103,7 +107,7 @@ def overplot(starfile):
     color = ['b', 'g', 'y', 'm']
 
     datafiles = sorted([s for s in os.listdir(dir) if s.startswith('obs')])
-    datafiles = datafiles[800:]
+    datafiles = datafiles[-1:]
     for i, datafile in enumerate(datafiles):
         if i % 20 == 0:
             print(i)
@@ -111,7 +115,7 @@ def overplot(starfile):
         l, n, f, q = np.loadtxt(path, usecols=(0, 1, 2, 3)).T
 
         dnu = np.median(np.diff(f[l == 0]))
-        h = echelle(starfile, dnu)
+        h, plot_position = echelle(starfile, dnu)
 
         # fcorr, llist = kjeldsen_corr(l, n, f, q, dnu, nu0, n_obs,
         #   l_obs, f_obs, dnu_obs)
@@ -122,15 +126,15 @@ def overplot(starfile):
         # closestfl0_list[i] = fl0[closestfl0_index]
         closestfl0_list.append(min(fl0, key=lambda p: abs(p - fl0_obs[0])))
 
-        plt.plot(np.mod(fl0_obs[0], dnu), fl0_obs[0], 'ro')
-        plt.plot(np.mod(closestfl0_list[i], dnu), closestfl0_list[i], 'md')
-        plt.plot(np.mod(fl0, dnu), fl0, 'bo')
-        plt.plot(np.mod(fl0_obs, dnu), fl0_obs, 'b', marker='d')
+        plt.plot(*plot_position(fl0_obs[0]), 'ro')
+        plt.plot(*plot_position(closestfl0_list[i]), 'md')
+        plt.plot(*plot_position(fl0), 'bo')
+        plt.plot(*plot_position(fl0_obs), 'b', marker='d')
         # for abe, kat in zip(fcorr, llist):
-        #    plt.plot(np.mod(abe, dnu), abe, color=color[kat], marker='d')
-        plt.plot(np.mod(f[l == 1], dnu), f[l == 1], 'go')
-        # plt.plot(np.mod(f[l == 2], dnu), f[l == 2], 'yo')
-        # plt.plot(np.mod(f[l == 3], dnu), f[l == 3], 'mo')
+        #    plt.plot(*plot_position(abe), color=color[kat], marker='d')
+        plt.plot(*plot_position(f[l == 1]), 'go')
+        # plt.plot(*plot_position(f[l == 2]), 'yo')
+        # plt.plot(*plot_position(f[l == 3]), 'mo')
         plt.savefig('./echelle/%s_echelle_%s_%s.pdf' % (starname, i, dnu))
         plt.close()
     minfl0 = min(closestfl0_list)
@@ -150,23 +154,33 @@ def echelle(filename, delta_nu, save=None):
 
     startorder = nmax - 9
     endorder = nmax + 9
+    print("%s pixel rows of %s pixels" % (endorder-startorder, nx))
 
     start = int(startorder * nx)
     endo = int(endorder * nx)
     apower = power[start:endo]
     pixeldata = np.reshape(apower, (-1, nx))
+    
+    def plot_position(freqs):
+        o = freqs - freq[start]
+        x = o % dnu
+        y = start * fres + dnu * np.floor(o / dnu)
+        return x, y
 
     h = plt.figure()
-    plt.subplots_adjust(left=0.12, right=0.95, bottom=0.12, top=0.90)
+    fix_margins()
     plt.xlabel(r'Frequency mod $\Delta\nu=$ %s [$\mu$Hz]' % dnu)
     plt.ylabel(r'Frequency [$\mu$Hz]')
-    plt.xlim([0, dnu])
-    plt.ylim([start * fres, endo * fres])
+    # Subtract half a pixel in order for data points to show up
+    # in the middle of the pixel instead of in the lower left corner.
+    plt.xlim([-fres/2, dnu-fres/2])
+    plt.ylim([start * fres - dnu/2, endo * fres - dnu/2])
     plt.imshow(-pixeldata, aspect='auto', cmap='gray',
                interpolation='gaussian', origin='lower',
-               extent=(0, dnu, start * fres, endo * fres))
+               extent=(-fres/2, dnu-fres/2,
+                       start * fres - dnu/2, endo * fres - dnu/2))
     if save is not None:
         plt.savefig('./%s_echelle_%s.pdf' % ('181096', delta_nu))
-    return h
+    return h, plot_position
 
 overplot('181096.txt')
