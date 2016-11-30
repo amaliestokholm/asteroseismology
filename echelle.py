c83784870a3693a6b1eeff34b5812f49486a5832
Amalie Stokholm <stokholm@post.au.dk>
2016-11-30 21:57:00 +0100

    Changed a whole lot of stuff

--- echelle.py
+++ echelle.py
@@ -3,6 +3,7 @@ import numpy as np
 import scipy
 import matplotlib
 import seaborn as sns
+from collections import namedtuple
 
 
 def matplotlib_setup():
@@ -24,121 +25,152 @@ def matplotlib_setup():
                   preamble=r'\usepackage[T1]{fontenc}\usepackage{lmodern}')
 
 matplotlib_setup()
+
 import matplotlib.pyplot as plt
 
 
 def fix_margins():
-    plt.subplots_adjust(left=0.12, right=0.95, bottom=0.14, top=0.90)
+    plt.subplots_adjust(left=0.12, right=0.95, bottom=0.15, top=0.95)
 
 
-def kjeldsen_corr(l, n, f, q, dnu, nu0, obs_n, obs_l, obs_f, delta_nu):
+Modes = namedtuple('Modes', 'l n f q dnu'.split())
+
+
+def kjeldsen_corr(model_modes, observed_modes):
+    l, n, f, q, dnu = model_modes
+    n_obs, l_obs, f_obs, _, dnu_obs = observed_modes
     # Kjeldsen correction
-    b = 4.90
-
+    # Correcting stellar oscillation frequencies for
+    # near-surface effects, Kjeldsen et al., 2008
+    b = 4.90  # from a solar model
+    nu0 = 1000
+    print('kjeldsen')
     output = []
     llist = []
     i_l0 = q[l == 0]
     nl0 = n[l == 0]
-    plt.figure()
-    # plt.subplots_adjust(left=0.12, right=0.95, bottom=0.12, top=0.90)
 
-    sns.set_style("darkgrid")
+    plt.figure()
     plt.xlabel(r'$\nu_{\text{model}}$ [$\mu$Hz]')
     plt.ylabel(r'$\nu-\nu_{\text{model}}$ [$\mu$Hz]')
     color = ['b', 'g', 'y', 'm']
-    uniquelist = np.unique(obs_l)
-    for k in uniquelist[:-1]:
+    ls_obs = np.unique(l_obs)
+    for k in ls_obs[:-1]:
         print('l=%s' % k)
-        obs_fl = obs_f[obs_l == k]
-        obs_nl = obs_n[obs_l == k]
-        assert len(obs_nl) == len(np.unique(obs_nl))
+        fl_obs = f_obs[l_obs == k]
+        nl_obs = n_obs[l_obs == k]
+        assert len(nl_obs) == len(np.unique(nl_obs))
+
         fl = f[l == k]
         nl = n[l == k]
-        qsl = q[l == k]
-        fobs = []
-        fbest = []
+        ql = q[l == k]
+        f_obs = []
+        f_ref = []
         qlist = []
 
-        ns = set(nl) & set(obs_nl)
+        ns = set(nl) & set(nl_obs)
         for m in sorted(ns):
-            aelement, = obs_fl[obs_nl == m]
+            element_obs, = fl_obs[nl_obs == m]
             element, = fl[nl == m]
-            fobs.append(aelement)
-            fbest.append(element)
-            qs, = qsl[nl == m]
+            f_obs.append(element_obs)
+            f_ref.append(element)
+            qnl, = ql[nl == m]
             i_l0s, = i_l0[nl0 == m]
-            qs = qs / i_l0s
+            qs = qnl / i_l0s
             qlist.append(qs)
-        fbest = np.asarray(fbest)
-        fobs = np.asarray(fobs)
+        f_ref = np.asarray(f_ref)
+        f_obs = np.asarray(f_obs)
         qlist = np.asarray(qlist)
         r = ((b - 1) *
-             (b * ((fbest) / (fobs)) - ((dnu) / (delta_nu))) ** (-1))
-        a = ((np.mean(fobs) - r * np.mean(fbest)) /
-             (len(fobs) ** (-1) * np.sum((fobs / nu0) ** b)))
-        fcorr = (fbest + a * (fbest / nu0) ** b) / qlist
-        output.append(fcorr)
+             (b * ((f_ref) / (f_obs)) - ((dnu) / (dnu_obs))) ** (-1))
+        print('r=%s' % r)
+        a = ((np.mean(f_obs) - r * np.mean(f_ref)) /
+             (len(f_obs) ** (-1) * np.sum((f_obs / nu0) ** b)))
+        f_corr = (f_ref + (1 / qlist) * (a / r) * (f_ref / nu0) ** b)
+        output.append(f_corr)
         k = int(k)
         llist.append(k)
         print(k, color[k])
-        plt.plot(fbest, (fobs - fbest), color=color[k], marker='d')
-        plt.plot(fbest, (fcorr - fbest), color=color[k], marker='o')
+        plt.plot(f_ref, (f_obs - f_ref), color=color[k], marker='d')
+        plt.plot(f_ref, (f_corr - f_ref), color=color[k], marker='o')
 
     plt.savefig('./echelle/kjeldsen_%s.pdf' % (dnu))
     plt.close()
     return output, llist
 
 
-def chisqr(obs_freq, corr_freq):
-    N = len(obs_freq)
-    sigma_obs_freq = 5
-    return ((1 / N) * np.sum(((obs_freq - corr_freq) /
-            (sigma_obs_freq)) ** 2))
+def chisqr(f_obs, f_corr):
+    N = len(f_obs)
+    sigma_f_obs = 1  # could be read from mikkelfreq and find median
+    f_corr = f_ref + (1/q) * (a/r) * ((f_obs)/f0) ** b
+    return ((1 / N) * np.sum(((f_corr - f_obs) /
+            (sigma_f_obs)) ** 2))
 
 
-def overplot(starfile):
+def overplot(job, starfile, obsfile, dnu_obs):
     starname = starfile.replace('.txt', '')
-    n_obs, l_obs, f_obs = np.loadtxt('amaliefreq.txt').T
-    dnu_obs = 53.8
-    fl0_obs = sorted(f_obs[l_obs == 0])
+    n_obs, l_obs, f_obs, q_obs = np.loadtxt(
+        obsfile, skiprows=1, usecols=(0, 1, 2, 3)).T
     closestfl0_list = []
-    dir = './models/X072669_Y02628_nor/freqs/'
-
-    color = ['b', 'g', 'y', 'm']
+    dir = './%s/X072669_Y02628_nor/freqs/' % job
+    fl0_obs = np.array(sorted(f_obs[l_obs == 0]))
+    nl0_obs = np.array(sorted(n_obs[l_obs == 0]))
 
     datafiles = sorted([s for s in os.listdir(dir) if s.startswith('obs')])
-    datafiles = datafiles[-1:]
+    # datafiles = datafiles[27:30]
+    observed_modes = Modes(n_obs, l_obs, f_obs, q_obs, dnu_obs)
     for i, datafile in enumerate(datafiles):
         if i % 20 == 0:
             print(i)
         path = os.path.join(dir, datafile)
         l, n, f, q = np.loadtxt(path, usecols=(0, 1, 2, 3)).T
-
         dnu = np.median(np.diff(f[l == 0]))
-        h, plot_position = echelle(starfile, dnu)
+        model_modes = Modes(l, n, f, q, dnu)
 
-        # fcorr, llist = kjeldsen_corr(l, n, f, q, dnu, nu0, n_obs,
-        #   l_obs, f_obs, dnu_obs)
+        h, plot_position = echelle(starfile, observed_modes.dnu)
 
-        fl0 = sorted(f[l == 0])
+        fcorr, llist = kjeldsen_corr(model_modes,
+                                     observed_modes)
+        """
+        fl0 = np.array(sorted(f[l == 0]))
+        nl0 = np.array(sorted(n[l == 0]))
         # closestfl0_index = (min(range(len(fl0)),
         #                     key=lambda x: abs(fl0[x]-fl0_obs[0])))
         # closestfl0_list[i] = fl0[closestfl0_index]
-        closestfl0_list.append(min(fl0, key=lambda p: abs(p - fl0_obs[0])))
-
-        plt.plot(*plot_position(fl0_obs[0]), 'ro')
-        plt.plot(*plot_position(closestfl0_list[i]), 'md')
-        plt.plot(*plot_position(fl0), 'bo')
-        plt.plot(*plot_position(fl0_obs), 'b', marker='d')
+        # closestfl0_list.append(min(fl0, key=lambda p: abs(p - fl0_obs[0])))
+        closestfl0_list.append(fl0[nl0 == nl0_obs[0]])
+        l0color = 'tomato'  # 'lightcoral'
+        l1color = 'firebrick'  # 'crimson'
+        plt.plot(*plot_position(closestfl0_list[i]), 'o',
+                 color=l0color, markersize=7,
+                 label=r'lowest, closest $\nu$ with $l=0$')
+        plt.plot(*plot_position(fl0_obs[0]), 'd',
+                 color=l0color, markersize=7,
+                 label=r'lowest, closest $\nu_{{obs}}$ with $l=0$')
+        plt.plot(*plot_position(fl0), 'o', markersize=7,
+                 markeredgewidth=1, markeredgecolor=l0color,
+                 markerfacecolor='none', label=r'$\nu$ with $l=0$')
+        plt.plot(*plot_position(fl0_obs), 'd', markersize=7,
+                 markeredgewidth=1, markeredgecolor=l0color,
+                 markerfacecolor='none', label=r'$\nu_{{obs}}$ with $l=0$')
         # for abe, kat in zip(fcorr, llist):
         #    plt.plot(*plot_position(abe), color=color[kat], marker='d')
-        plt.plot(*plot_position(f[l == 1]), 'go')
+        # plt.plot(*plot_position(f[l == 1]), 'o', markersize=7,
+        #         markeredgewidth=1, markeredgecolor=l1color,
+        #         markerfacecolor='none', label=r'$\nu$ with $l=1$')
         # plt.plot(*plot_position(f[l == 2]), 'yo')
         # plt.plot(*plot_position(f[l == 3]), 'mo')
-        plt.savefig('./echelle/%s_echelle_%s_%s.pdf' % (starname, i, dnu))
+        # h.legend(loc='upper center', bbox_to_anchor=(0.5, 1.05),
+        #          ncol=3, fancybox=True, shadow=True)
+        plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3, ncol=2,
+                   mode="expand", borderaxespad=0., frameon=False)
+        plt.savefig('./echelle/%s/%s_echelle_%s_%s.pdf' %
+                    (job, starname, i, dnu), bbox_inches='tight')
         plt.close()
-    minfl0 = min(closestfl0_list)
+        print(closestfl0_list[i], n[closestfl0_list[i] == f], fl0_obs[0])
+    minfl0 = min(closestfl0_list, key=lambda p: abs(p - fl0_obs[0]))
     print(closestfl0_list.index(minfl0), minfl0, fl0_obs[0])
+    """
 
 
 def echelle(filename, delta_nu, save=None):
@@ -148,19 +180,20 @@ def echelle(filename, delta_nu, save=None):
     numax = (delta_nu / 0.263) ** (1 / 0.772)
     nmax = int(np.round(((numax - freq[0]) / delta_nu) - 1))
     nx = int(np.round(delta_nu / fres))
+    assert nx % 2 == 0  # we shift by nx/2 pixels below
     dnu = nx * fres
 
     ny = int(np.floor(len(power) / nx))
 
     startorder = nmax - 9
     endorder = nmax + 9
-    print("%s pixel rows of %s pixels" % (endorder-startorder, nx))
+    # print("%s pixel rows of %s pixels" % (endorder-startorder, nx))
 
-    start = int(startorder * nx)
-    endo = int(endorder * nx)
+    start = int(startorder * nx + nx/2)
+    endo = int(endorder * nx + nx/2)
     apower = power[start:endo]
     pixeldata = np.reshape(apower, (-1, nx))
-    
+
     def plot_position(freqs):
         o = freqs - freq[start]
         x = o % dnu
@@ -169,18 +202,21 @@ def echelle(filename, delta_nu, save=None):
 
     h = plt.figure()
     fix_margins()
-    plt.xlabel(r'Frequency mod $\Delta\nu=$ %s [$\mu$Hz]' % dnu)
+    plt.xlabel(r'Frequency mod $\Delta\nu -\Delta\nu/2$ with $\Delta\nu=$ %s [$\mu$Hz]' % dnu)
     plt.ylabel(r'Frequency [$\mu$Hz]')
     # Subtract half a pixel in order for data points to show up
     # in the middle of the pixel instead of in the lower left corner.
     plt.xlim([-fres/2, dnu-fres/2])
     plt.ylim([start * fres - dnu/2, endo * fres - dnu/2])
-    plt.imshow(-pixeldata, aspect='auto', cmap='gray',
+    """
+    plt.imshow(pixeldata, aspect='auto', cmap='Blues',
                interpolation='gaussian', origin='lower',
                extent=(-fres/2, dnu-fres/2,
                        start * fres - dnu/2, endo * fres - dnu/2))
+    """
     if save is not None:
-        plt.savefig('./%s_echelle_%s.pdf' % ('181096', delta_nu))
+        plt.savefig('./%s_echelle_%s.pdf' % ('181096', delta_nu),
+                    bbox_inches='tight')
     return h, plot_position
 
-overplot('181096.txt')
+overplot('amalie2', '181096.txt', 'mikkelfreq.txt', 53.8)
